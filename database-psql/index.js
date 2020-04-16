2d59636bc4454eab7905706222152b0718bf7343
Brandon Gomez <BrandonJosephGomez@gmail.com>
2020-04-15 21:56:06 -0600

    updated

--- database-psql/index.js
+++ database-psql/index.js
@@ -1,6 +1,6 @@
 // const mysql = require('mysql')
 const pg = require('pg');
-const connectionString = "postgres://postgres:postgres@54.149.162.127:5432/nykeproducts"
+const connectionString = "postgres://postgres:@54.149.162.127:5432/nykeproducts"
 var pgClient = new pg.Client(connectionString);
 pgClient.connect()
 .then(()=> {
